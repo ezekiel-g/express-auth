ca5de05b8d26c6eec2af0a588784b2a02c44e304
ezekiel-g <40302800+ezekiel-g@users.noreply.github.com>
2025-05-13 20:34:19 -0400

    Added validateSession utility and static path

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import userRoutes from './api/v1/routes/userRoutes.js'
 import sessionRoutes from './api/v1/routes/sessionRoutes.js'
 
@@ -20,6 +22,18 @@ app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/sessions', sessionRoutes)
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+if (process.env.NODE_ENV === 'production') {
+    const clientDistPath = path.join(__dirname, '../client/dist')
+    app.use(express.static(clientDistPath))
+
+    app.get('*', (request, response) => {
+        response.sendFile(path.join(clientDistPath, 'index.html'))
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
