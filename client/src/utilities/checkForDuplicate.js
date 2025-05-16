eb364f9bbe83f03ba12383f705435292078e3cb1
ezekiel-g <40302800+ezekiel-g@users.noreply.github.com>
2025-05-15 22:00:16 -0400

    Refactored checkForDuplicate

--- client/src/utilities/checkForDuplicate.js
+++ client/src/utilities/checkForDuplicate.js
@@ -1,12 +1,14 @@
 const checkForDuplicate = async (
-    value,
-    columnName,
+    entryObject,
     fetchFunction,
     excludeId = null
 ) => {
     const rows = await fetchFunction()
 
     if (rows.length === 0 || !Array.isArray(rows)) return 'pass'
+    
+    const columnName = Object.keys(entryObject)[0]
+    const value = entryObject[columnName]
 
     const hasDuplicate = rows.some(
         row => row[columnName] === value && row.id !== excludeId
--- server/api/v1/utilities/checkForDuplicates.js
+++ server/api/v1/utilities/checkForDuplicates.js
@@ -12,11 +12,11 @@ const queries = {
 }
 
 const checkForDuplicates = async (response, entries = {}, excludeId = null) => {
-    const keys = Object.keys(entries)
+    const columnNames = Object.keys(entries)
     const duplicateEntryErrors = []
     
-    for (let i = 0; i < keys.length; i++) {
-        const columnName = keys[i]
+    for (let i = 0; i < columnNames.length; i++) {
+        const columnName = columnNames[i]
         const value = entries[columnName]
         
         if (!value) continue
