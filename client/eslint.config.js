4761bd31f5570730c22fe819964cffb2feb2be81
ezekiel-g <40302800+ezekiel-g@users.noreply.github.com>
2025-06-04 08:47:52 -0400

    Changed eslint rules to treat react components as used

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
@@ -17,11 +18,18 @@ export default [
             }
         },
         plugins: {
+            react,
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh
         },
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        },
         rules: {
             ...js.configs.recommended.rules,
+            ...react.configs.recommended.rules,
             ...reactHooks.configs.recommended.rules,
             indent: ['error', 4],
             semi: ['error', 'never'],
@@ -36,7 +44,8 @@ export default [
             'react-refresh/only-export-components': [
                 'warn',
                 { allowConstantExport: true }
-            ]
+            ],
+            'react/react-in-jsx-scope': 'off'
         }
     }
 ]
--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,14 +1,36 @@
+import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
-const lintConfig = [
+export default [
+    { ignores: ['dist'] },
     {
         files: ['**/*.{js,jsx,ts,tsx}'],
         languageOptions: {
             ecmaVersion: 'latest',
-            globals: globals.node,
-            sourceType: 'module'
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType: 'module'
+            }
+        },
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh
+        },
+        settings: {
+            react: {
+                version: 'detect'
+            }
         },
         rules: {
+            ...js.configs.recommended.rules,
+            ...react.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
             indent: ['error', 4],
             semi: ['error', 'never'],
             quotes: ['error', 'single'],
@@ -18,9 +40,12 @@ const lintConfig = [
             'prefer-const': 'warn',
             'no-var': 'error',
             'no-unused-vars': 'warn',
-            'no-console': 'off'
+            'no-console': 'off',
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true }
+            ],
+            'react/react-in-jsx-scope': 'off'
         }
     }
 ]
-
-export default lintConfig
